5afd7b842601cceeb2160725f295a224845e0eaa
Platelets <wekanedward@hust.edu.cn>
2024-04-23 17:54:52 +0800

    remove GCD main object

--- src/main/scala/gcd/GCD.scala
+++ src/main/scala/gcd/GCD.scala
@@ -39,9 +39,9 @@ class GCD extends Module {
 /**
  * Generate Verilog sources and save it in file GCD.v
  */
-object GCD extends App {
-  ChiselStage.emitSystemVerilogFile(
-    new GCD,
-    firtoolOpts = Array("-disable-all-randomization", "-strip-debug-info")
-  )
-}
+// object GCD extends App {
+//   ChiselStage.emitSystemVerilogFile(
+//     new GCD,
+//     firtoolOpts = Array("-disable-all-randomization", "-strip-debug-info")
+//   )
+// }
