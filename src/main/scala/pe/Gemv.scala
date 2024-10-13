7a6ab5713f3fb8b07141c6a84fe68aa6c464c90a
platelets <wenkanedward@hust.edu.cn>
2024-10-13 23:45:19 +0800

    add vecdotvec with UInt and Fxp, without any tests

---
+++ src/main/scala/pe/Gemv.scala
@@ -0,0 +1,45 @@
+package pe
+
+import chisel3._
+import pe.utils.FxpAddSub
+import pe.utils.FxpMul
+import pe.utils.common
+
+class VecDotVec(val size: Int = 4, val bits: Int = 8) extends Module {
+  val io = IO(new Bundle {
+    val vecA = Input(Vec(size, UInt(bits.W)))
+    val vecB = Input(Vec(size, UInt(bits.W)))
+    val res = Output(UInt((bits * 2 + 1).W))
+  })
+
+  val sumMultiply = VecInit((io.vecA.zip(io.vecB)).map { case (a, b) => a * b })
+  io.res := sumMultiply.reduceTree((a, b) => RegNext(a +& b))
+}
+
+class VecDotVecFxp(val size: Int = 4, val WII: Int, val WIF: Int, val WOI: Int, val WOF: Int) extends Module {
+  val vecA = IO(Input(Vec(size, UInt((WII + WIF).W))))
+  val vecB = IO(Input(Vec(size, UInt((WII + WIF).W))))
+
+  val res = IO(Output(UInt((WOI + WOF).W)))
+
+  val FxpMulVec = VecInit(
+    (vecA.zip(vecB)).map {
+      case (a, b) => {
+        val mul = Module(new FxpMul(WII, WIF, WII, WIF, WII, WIF))
+        mul.io.ina <> a
+        mul.io.inb <> b
+        mul.io.out
+      }
+    }
+  )
+
+  res := FxpMulVec.reduceTree((a, b) => {
+    val add = Module(new FxpAddSub(WII, WIF, WII, WIF, WOI, WOF))
+    add.io.ina <> a
+    add.io.inb <> b
+    add.io.sub <> common.AddOrSub.ADD
+    add.io.overflow := DontCare
+    add.io.out
+  })
+
+}
